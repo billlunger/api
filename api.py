f7dde9d71aae864d22935af00be88acf37c6a834
Bill <bill@Bills-MacBook-Pro.local>
2021-06-09 08:39:41 -0700

    added project description

--- api.py
+++ api.py
@@ -444,7 +444,18 @@ def convertPhases(df,locs,quoteId, lookP, lookB, lookM, phaseName,clientName,quo
         txt=open(f'{p}{clientName}-{quoteName}-{str(phaseName)}({date}).txt', "a")
     else:
         txt=open(f'{p}{clientName}-{quoteName}({date}).txt', "a")
-
+    
+######################## Write Project Description    
+    par = (('quoteId', quoteId),)
+    resp = requests.get('https://api.d-tools.cloud/Quote/api/v1/QuoteScopeOfWorks/GetQuoteScopeOfWork', headers=headers, params=par).json()
+    html1 = resp['content']
+    html1 = re.sub(r'</.*?>', '\n', html1)
+    html1 = re.sub(r'<ul>', '', html1)
+    html1 = re.sub(r'<p>', '    ', html1)
+    html1 = re.sub(r'<li>', '      *', html1)
+    html1 = re.sub(r'<li class.*?>', '        *', html1)
+    txt.write('Project Description: \n' + html1)
+######################## Write Scopes and Descriptions
     for i, row in df.iterrows():
         if 'Room' in row['itemId']:
             txt.write('\n' + row['shortDescription']+'\n')
